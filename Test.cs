be124b6572081aa75fac5fb8d8ef67b40d1b999a
Soumya1234 <soumyadeepganguly41@gmail.com>
2017-09-14 22:10:11 +0530

    First Commit

---
+++ Test.cs
@@ -0,0 +1,12 @@
+﻿/* This is my first .Net Application */
+/* This is a simple C# Application */
+
+using System;
+
+class Test
+{
+    static void Main()
+    {
+        Console.WriteLine("This is a simple C# Program");
+    }
+}
---
+++ Test2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Test2
+{
+    static void Main()
+    {
+        double x = 10.0;
+        double y = 3.0;
+        int z = (int)(x / y);
+        Console.WriteLine("Integer Outcome of x/y:" + z);
+        Console.WriteLine(sizeof(byte));
+
+        
+    }
+}
