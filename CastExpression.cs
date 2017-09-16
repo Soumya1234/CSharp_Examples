295a6c0cdee926ca5918b8cca08e40c27ca9ea37
Soumya1234 <soumyadeepganguly41@gmail.com>
2017-09-16 16:23:38 +0530

    Added CastExpression.cs file

---
+++ CastExpression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class CastExpression
+{
+    static void Main()
+    {
+        double n;
+        for(n=1.0;n<=10.0; n++)
+        {
+            Console.WriteLine("The square root of {0} is {1}", n, Math.Sqrt(n));
+            Console.WriteLine("The whole number part:" + (int)Math.Sqrt(n));
+            Console.WriteLine("The fractional part:" + (Math.Sqrt(n) - (int)Math.Sqrt(n)));
+            Console.WriteLine();
+        }
+    }
+}
